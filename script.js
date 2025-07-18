58ecf1c0c7156a2932f9a3ae23ea68a21c523604
Shivam Goyal <151446208+shivamgoyal9119@users.noreply.github.com>
2025-07-18 23:12:46 +0530

    Update script.js
    Update leaflet map loading link

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ class App {
     this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
     console.log(this.#map);
 
-    L.tileLayer("https://tile.openstreetmap.fr./hot/{z}/{x}/{y}.png", {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
